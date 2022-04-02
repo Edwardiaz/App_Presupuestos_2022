0e20746b208ed7f94b720fa071445437bc0e7764
Jorge Díaz <jorgediaz9515@gmail.com>
2022-04-02 17:38:52 -0600

    Commit 02
    Adapte el index mejor y añadi un par de scripts para simular unos calculos

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,14 @@
-const meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+function calcularFechaActual() {
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-const date = new Date();
-let mes = meses[date.getMonth()];
-//document.getElementById("mes").innerHTML = mes;
-let anio = date.getFullYear();
-document.getElementById("fecha").innerHTML = mes +' '+ anio;
+    const date = new Date();
+    let mes = meses[date.getMonth()];
+    //document.getElementById("mes").innerHTML = mes;
+    let anio = date.getFullYear();
+    document.getElementById("fecha").innerHTML = mes + ' ' + anio;
+}
+
+function calculoTest(){
+    document.getElementById("calcIngre").innerHTML = "+"+600;
+    document.getElementById("calcEgre").innerHTML = "-"+100;
+}
