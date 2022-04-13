d05dd283d04e577d1ae114532f46be0a0aa00bc8
Jorge Díaz <jorgediaz9515@gmail.com>
2022-04-13 16:46:26 -0600

    Commit 07
    Añadi el calculo para el total del titulo.

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,6 @@ function calcularFechaActual() {
 
     const date = new Date();
     let mes = meses[date.getMonth()];
-    //document.getElementById("mes").innerHTML = mes;
     let anio = date.getFullYear();
     document.getElementById("fecha").innerHTML = mes + ' ' + anio;
 }
@@ -12,23 +11,48 @@ function calculoTest(){
     document.getElementById("calcIngre").innerHTML = "+"+600;
     document.getElementById("calcEgre").innerHTML = "-"+100;
     document.getElementById("porcentajeGasto").innerHTML = 20+"%";
+
+}
+
+var total = 0
+if(document.getElementById("total").innerHTML == ''){
+    document.getElementById("total").innerHTML = "+"+0;
 }
 
 function agregar()
 {
-    var menu = document.getElementById("menu").value
-    var descripcion = document.getElementById("descripcion").value
-    var monto = document.getElementById("monto").value
+    /* Consigo los valores en los tags */
+    var menu = document.getElementById("menu").value; /* Para validar si es egreso o ingreso, aun no lo uso */
+    var descripcion = document.getElementById("descripcion").value;
+    var monto = document.getElementById("monto").value;
+    var tempTotal = document.getElementById("total").innerHTML;
+
+    if(monto==""){
+        monto = 0;
+    }
+
+    var valorActual = tempTotal.replace("+","");
+
+    if(valorActual==undefined || valorActual== ""){
+        valorActual= '0';
+    }
+
+    /* Hago la suma convirtiendo cada variable a decimal*/
+    total = parseFloat(valorActual) + parseFloat(monto);
+
+    /* creo el nodo de la nueva transaccion y que agregaré a la lista*/
     const lista = document.createElement("li");
-    
     const node = document.createTextNode(descripcion+" "+monto);
     lista.appendChild(node);
     const ulTag = document.getElementById("lista");
+
+    /* guardo el total como string para quitar algun simbolo extra*/
+    totalString = total.toString();
+
+    /*Agrego la transaccion a la lista*/
     ulTag.appendChild(lista);
-
+    document.getElementById("total").innerHTML = "+"+total;
+    
 }
 
-function calculoTotal(total){
-    document.getElementById("total").innerHTML = "+"+500;
-}
 
